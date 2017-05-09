b7d2acb04817593961f404b9bfd7842d635a7f12
vanuan <vanuan@gmail.com>
2017-05-10 00:11:15 +0300

    Fix piwik URL

--- indexHtml.js
+++ indexHtml.js
@@ -67,7 +67,7 @@ export default function(html, initialState, head) {
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-      var u="//46.101.102.160:9000/";
+      var u="/piwik/";
       _paq.push(['setTrackerUrl', u+'piwik.php']);
       _paq.push(['setSiteId', '1']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
