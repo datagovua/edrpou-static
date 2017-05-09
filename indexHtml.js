7df597673137b14ef8c78d3b89bf498bc8739520
vanuan <vanuan@gmail.com>
2017-05-09 23:12:35 +0300

    Add Piwik tracking

--- indexHtml.js
+++ indexHtml.js
@@ -58,6 +58,24 @@ export default function(html, initialState, head) {
   <script>
     SystemJS.import('browser.js');
   </script>
+
+
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//46.101.102.160:9000/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <!-- End Piwik Code -->
+
 </body>
 </html>`;
 }
